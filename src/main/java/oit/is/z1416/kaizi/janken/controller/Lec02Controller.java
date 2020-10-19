55d6ffd96a499d07e81536617470d2cad5761f85
nyaiQ <e1b18022@oit.ac.jp>
2020-10-19 19:59:27 +0900

    じゃんけんの手を選んでCPUと対戦する

--- src/main/java/oit/is/z1416/kaizi/janken/controller/Lec02Controller.java
+++ src/main/java/oit/is/z1416/kaizi/janken/controller/Lec02Controller.java
@@ -4,12 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Lec02Controller {
+
   @PostMapping("/lec02")
   public String lec02(@RequestParam String userName, ModelMap model) {
     model.addAttribute("userName", userName);
     return "lec02.html";
   }
+
+  @GetMapping("/lec02")
+  public String lec02_hand(@RequestParam String hand, ModelMap model) {
+    model.addAttribute("hand", hand);
+    return "lec02.html";
+  }
+
 }
