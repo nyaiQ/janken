273403b4d3f85d981d711a087c62bb880f20cb50
nyaiQ <e1b18022@oit.ac.jp>
2020-11-09 12:17:14 +0900

    DBの初期化

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    id IDENTITY,
+    user CHAR NOT NULL
+);
+CREATE TABLE matches (
+  id IDENTITY,
+  user_1 INT NOT NULL,
+  user_2 INT NOT NULL,
+  user_1_hand CHAR NOT NULL,
+  user_2_hand CHAR NOT NULL
+);
